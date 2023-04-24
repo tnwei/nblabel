b3d097ff20cd919d1af794e55e1efd5dbc5627e6
Tan Nian Wei <tannianwei@aggienetwork.com>
2023-04-24 11:36:46 +0800

    chore: added setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+from pathlib import Path
+
+projectroot = Path(__file__).parent.resolve()
+
+# To incorporate README as long desription:
+#
+# Uncomment below and relevant line in setuptools.setup
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+# To include version number:
+#
+# Single source version number using nblabel/VERSION.txt
+# Ensure that there is a MANIFEST.in file to include VERSION.txt in packaging
+# Comment below:
+version = "0.1.0"
+# Uncomment below, and relevant line in setuptools.setup
+# with open(projectroot / "nblabel/VERSION.txt", "r") as f:
+#     version = f.read().strip()
+
+setuptools.setup(
+    # Important args
+    name="nblabel",
+    version=version,
+    author="Tan Nian Wei",
+    description="Label tabular data directly in Jupyter Notebook / Lab",
+    packages=["nblabel"],
+    install_requires=[
+        "ipywidgets",
+        "bqplot",
+        "pandas",
+        "numpy",
+        "traitlets",
+    ],  # SPECIFY DEPENDENCIES HERE e.g. ["pandas", "numpy"]
+    # Less important args
+    # author_email="YOUR-EMAIL-HERE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tnwei/nblabel",
+    # python_requires=">=3.6", # SPECIFY REQUIRED PYTHON VERSION
+    # include_package_data=True # Uncomment if using MANIFEST.in
+)
